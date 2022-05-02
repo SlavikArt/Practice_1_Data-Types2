c6e1f0ed564c52918252cb03dda399c04a6f1110
Slavik-Art <slavik.adobe@gmail.com>
2022-05-02 17:57:14 +0300

    Добавьте файлы проекта.

---
+++ Celsius_To_ Fahrenheit/Celsius_To_ Fahrenheit.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "rus");
+
+    const float celsius_to_fahrengeit = 1.8;
+    const float fahrenheit_to_celsius = 1.8;
+
+    float celsius;
+    float fahrenheit;
+
+
+    cout << "Введите градусы в цельсиях: ";
+    cin >> celsius;
+
+    fahrenheit = celsius * celsius_to_fahrengeit + 32;
+
+    cout << celsius << " градусов цельсия в фаренгейтах это " << fahrenheit << "\n\n";
+
+    cout << "Введите градусы в фаренгейтах: ";
+    cin >> fahrenheit;
+
+    celsius = (fahrenheit - 32) / fahrenheit_to_celsius;
+
+    cout << fahrenheit << " градусов фаренгейта в цельсиях это " << celsius << "\n";
+
+
+
+
+
+
+
+}
